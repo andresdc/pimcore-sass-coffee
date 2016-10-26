7be7104b1d06b8d3f995ce35711130f4b7b04939
Andrés De Castro <af.decastro879@uniandes.edu.co>
2016-10-26 11:21:34 -0500

    Css and JS Minifiers

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ var $ = require('gulp-load-plugins')();
 var gulpsync = $.sync(gulp);
 var util = $.util;
 var coffee = require('gulp-coffee');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+
+var isProduction = false;
 
 gulp.task('default', gulpsync.sync([
     'sass',
@@ -19,25 +23,62 @@ gulp.task('default', gulpsync.sync([
 ]));
 
 gulp.task('sass', function () {
-    return gulp.src('./pimcore/static/sass/**/*.scss')
+    log('Converting SASS files to CSS');
+    return gulp.src('./pimcore/static/sass/**/*.sass')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./pimcore/static/css'));
+        .pipe(gulp.dest('./pimcore/static/css/custom'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./pimcore/static/sass/**/*.scss', ['sass']);
+    gulp.watch('./pimcore/static/sass/**/*.sass', ['sass']);
 });
 
 gulp.task('coffee', function () {
+    log('Converting Coffee files to JS ... ');
     gulp.src('./pimcore/static/coffee/**/*.coffee')
         .pipe(sourcemaps.init())
         .pipe(coffee({bare: true})).on('error', $.util.log)
-        .pipe(sourcemaps.write('./pimcore/static/coffee/maps'))
-        .pipe(gulp.dest('./pimcore/static/js'));
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./pimcore/static/js/custom'));
 });
 
 gulp.task('coffee:watch', function () {
     gulp.watch('./pimcore/static/coffee/**/*.coffee', ['coffee']);
 });
+
+gulp.task('minify:js', function () {
+    log('Generating Minified Javascripts ... ');
+    // Minify and copy all JavaScript (except vendor scripts)
+    return gulp.src(['./pimcore/static/js/custom/**/*.js'])
+        .pipe($.jsvalidate())
+        .on('error', handleError)
+        .pipe($.uglify({preserveComments: 'some'}))
+        .on('error', handleError)
+        .pipe(rename({
+            extname: ".min.js"
+        }))
+        .pipe(gulp.dest('./pimcore/static/js/custom-min'));
+});
+
+gulp.task('minify:css', function () {
+    log('Generating Minified CSS ... ');
+    return gulp.src(['./pimcore/static/css/custom/**/*.css'])
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({
+            extname: ".min.css"
+        }))
+        .pipe(gulp.dest('./pimcore/static/css/custom-min'));
+});
+
+// Error handler
+function handleError(err) {
+    log(err.toString());
+    this.emit('end');
+}
+
+// log to console using
+function log(msg) {
+    $.util.log($.util.colors.blue(msg));
+}
